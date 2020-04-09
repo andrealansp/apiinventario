40f5860f46ce44c3dbb09aedb6335c92c62870d7
Andre Alves <andrealansp@hotmail.com>
2020-04-08 22:01:16 -0300

    Implementação de Interface Gráfica

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from resources.equipamentos import Equipamentos, Equipamento, Consultas
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="views")
 api = Api(app)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 
 api.add_resource(Equipamentos, '/equipamentos')
 api.add_resource(Equipamento, '/equipamento/<string:eid>')
 api.add_resource(Consultas, '/equipamento/consulta/<string:ativo>')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='192.168.0.13')
